4c024f969e015b06f556fbf635becb09eb4b4205
It's Me Vishal <vishalkurrey950+1@gmail.com>
2025-10-14 19:17:43 +0530

    Add files via upload

---
+++ VishalMusic/plugins/misc/watcher.py
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from VishalMusic import app
+from VishalMusic.core.call import Nand
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await Nand.stop_stream_force(message.chat.id)
+
+
+
+
+
+
+
+
+
+
+
+
